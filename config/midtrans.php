defac4ae57ca1ff2f7febcaaee6d7a12a3d4d844
AulaHamidin <aulahamidin04@gmail.com>
2025-08-01 11:13:31 +0700

    Add Midtrans payment configuration and update composer dependencies

---
+++ config/midtrans.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'midtrans_server_key' => env('MIDTRANS_MIDTRANS_SERVER_KEY'),
+    'midtrans_client_key' => env('MIDTRANS_MIDTRANS_CLIENT_KEY'),
+    'midtrans_is_production' => env('MIDTRANS_MIDTRANS_IS_PRODUCTION'),
+    'merchant_id' => env('MIDTRANS_MERCHANT_ID'),  
+];
